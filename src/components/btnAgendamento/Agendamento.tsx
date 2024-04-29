b44226c9b47ed3900ae2681518b23a477768f1a5
DESKTOP-FUQ1C3A\HOME <binho.bgu@gmail.com>
2024-04-29 16:28:44 -0300

    inclusao link wpp

--- src/components/btnAgendamento/Agendamento.tsx
+++ src/components/btnAgendamento/Agendamento.tsx
@@ -4,7 +4,7 @@ const Agendamento: React.FC<{ text: string }> = ({ text }) => {
   return (
     <button className="hover:scale-110 duration-500 text-zinc-900 font-bold">
       <a
-        href="http://"
+        href="https://wa.me/5544991269022"
         target="_blank"
         rel="noopener noreferrer"
         className=" bg-btnContato p-3 rounded-xl"
