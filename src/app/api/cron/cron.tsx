696b10456fb6e2aaee2ec483a0c7f3745500da88
DESKTOP-FUQ1C3A\HOME <binho.bgu@gmail.com>
2024-07-03 16:40:54 -0300

    Mudando configuração cron

--- src/app/api/cron/cron.tsx
+++ src/app/api/cron/cron.tsx
@@ -1,20 +1,30 @@
-import { getYouTubeVideos } from '@/components/canal/YouTubeVideos';
+import { getYouTubeVideos } from '../../../components/canal/YouTubeVideos';
 
 // Função de endpoint para revalidar cache dos vídeos do YouTube
 export async function GET() {
-  try {
-    // Chama a função que busca os vídeos do YouTube
-    const videos = await getYouTubeVideos();
+  type YouTubeResponse = {
+    items: Video[];
+  };
 
-    // Loga os dados recebidos (isso pode ser substituído por qualquer ação que você queira tomar com os dados)
+  type Video = {
+    id: {
+      kind: string;
+      videoId: string;
+    };
+    snippet: {
+      title: string;
+    };
+  };
+
+  try {
+    const videos: YouTubeResponse = await getYouTubeVideos();
+
     console.log('Vídeos atualizados:', videos);
 
-    // Retorna uma resposta JSON com os vídeos
     return new Response(JSON.stringify(videos), {
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (error) {
-    // Loga o erro e retorna uma resposta de erro apropriada
     console.error('Erro ao atualizar vídeos:', error);
     return new Response('Erro ao atualizar vídeos', {
       status: 500,
