4615dbc9df86ede2827e8ee684fdb89980951ad8
DESKTOP-FUQ1C3A\HOME <binho.bgu@gmail.com>
2024-11-30 03:50:54 -0300

    configuração cron job

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -35,7 +35,14 @@ import { NextRequest, NextResponse } from 'next/server';
 // }
 
 export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
+
   console.log('Con job ram at: ', new Date());
 
-  return new NextResponse('cron ran', { status: 200 });
+  return NextResponse.json({ message: 'Cron Job Ran at ' + new Date() });
 }
